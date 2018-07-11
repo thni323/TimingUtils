783c2591d550aeb5cce83eeca94a3a1f96654cf1
thni <thni323@web.de>
2018-07-11 22:14:51 +0200

    Added example for callback timer

---
+++ example/callback_timer.cpp
@@ -0,0 +1,33 @@
+#include <Arduino.h>
+#include <CallbackTimer.h>
+
+CallbackTimer timerA, timerB;
+
+void callbackA() {
+    // Code placed here will be executed every 1000 ms
+    Serial.println("Callback Timer A");
+}
+
+void callbackB() {
+    // Code placed here will be executed every 2000 ms
+    Serial.println("Callback Timer B");
+}
+
+void setup() {
+    Serial.begin(9600);
+
+    timerA.setInterval(1000);
+    timerA.setCallback(callbackA);
+
+    timerB.setInterval(2000);
+    timerB.setCallback(callbackB);
+
+    CallbackTimer::init();
+
+    timerA.start();
+    timerB.start();
+}
+
+void loop() {
+    CallbackTimer::process();
+}
